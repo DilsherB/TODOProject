00b814d229a5c60212f12f2648126ee5fd160679
Dilsher Balouch <dilsherb@gmail.com>
2023-01-28 12:19:58 +0300

    linter errors resolved.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import './style.css';
 
 const toDoTasks = [
-    { description: 'first task', completed: false, index: 1 },
-    { description: 'second task', completed: false, index: 2 },
-    { description: 'third task', completed: true, index: 3 },
+  { description: 'first task', completed: false, index: 1 },
+  { description: 'second task', completed: false, index: 2 },
+  { description: 'third task', completed: true, index: 3 },
 ];
 
 
 const dynamicData = document.querySelector('.dynamicData');
 toDoTasks.forEach((task) => {
-    if (task.completed) {
-        dynamicData.innerHTML += `
+  if (task.completed) {
+    dynamicData.innerHTML += `
             <li class="deleteIcon"> 
                 <span><input type="checkbox" id="${task.index}" checked> 
                     <span class="lm" contenteditable="true"> ${task.description}</span>
@@ -19,11 +19,11 @@ toDoTasks.forEach((task) => {
                 <i class="fa-solid fa-trash-can trash hidden"></i>
             </li>
         `
-    } else {
-        dynamicData.innerHTML += `
+  } else {
+    dynamicData.innerHTML += `
             <li class="deleteIcon"> <span><input type="checkbox" id="${task.index}"> <span class="lm" contenteditable="true"> ${task.description}</span></span><i class="fa-solid fa-ellipsis-vertical dots"></i><i class="fa-solid fa-trash-can trash hidden"></i></li>
         `
-    }
+  }
 })
 
 const trash = document.querySelectorAll('.trash');
@@ -31,13 +31,13 @@ const dots = document.querySelectorAll('.dots');
 const hideShow = document.querySelectorAll('.deleteIcon');
 
 for (let i = 0; i < hideShow.length; i++) {
-    hideShow[i].addEventListener('mouseover', () => {
-        trash[i].classList.remove('hidden');
-        dots[i].classList.add('hidden');
-    });
-    hideShow[i].addEventListener('mouseout', () => {
-        trash[i].classList.add('hidden');
-        dots[i].classList.remove('hidden');
-    });
+  hideShow[i].addEventListener('mouseover', () => {
+    trash[i].classList.remove('hidden');
+    dots[i].classList.add('hidden');
+  });
+  hideShow[i].addEventListener('mouseout', () => {
+    trash[i].classList.add('hidden');
+    dots[i].classList.remove('hidden');
+  });
 }
 
