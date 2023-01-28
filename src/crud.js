76c814b193b8ad39ceac6b223471f1491ecbd54d
Dilsher Balouch <dilsherb@gmail.com>
2023-01-28 16:58:18 +0300

    linter errors resolved

--- src/crud.js
+++ src/crud.js
@@ -1,12 +1,11 @@
-import './index.js';
-import { toDoTasks, ui } from './index.js'
+import { toDoTasks } from './index.js'; // eslint-disable-line
 
 document.querySelector('.fa-angle-down').addEventListener('click', () => {
   const todoData = document.querySelector('.newTodo').value;
   if (todoData.trim() === '') {
     return;
   }
-  toDoTasks.push({ description: `${todoData}`, completed: false, index: toDoTasks.length })
+  toDoTasks.push({ description: `${todoData}`, completed: false, index: toDoTasks.length });
   localStorage.setItem('newTodo', JSON.stringify(toDoTasks));
   window.location.reload();
-})
+});
