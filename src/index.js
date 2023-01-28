d93c06e9fadf4b6d33d40ffd749dab54c9cb6136
Dilsher Balouch <dilsherb@gmail.com>
2023-01-28 12:22:54 +0300

    index.js linter issues resolved.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const toDoTasks = [
   { description: 'third task', completed: true, index: 3 },
 ];
 
-
 const dynamicData = document.querySelector('.dynamicData');
 toDoTasks.forEach((task) => {
   if (task.completed) {
@@ -18,19 +17,19 @@ toDoTasks.forEach((task) => {
                 <i class="fa-solid fa-ellipsis-vertical dots"></i>
                 <i class="fa-solid fa-trash-can trash hidden"></i>
             </li>
-        `
+        `;
   } else {
     dynamicData.innerHTML += `
             <li class="deleteIcon"> <span><input type="checkbox" id="${task.index}"> <span class="lm" contenteditable="true"> ${task.description}</span></span><i class="fa-solid fa-ellipsis-vertical dots"></i><i class="fa-solid fa-trash-can trash hidden"></i></li>
-        `
+        `;
   }
-})
+});
 
 const trash = document.querySelectorAll('.trash');
 const dots = document.querySelectorAll('.dots');
 const hideShow = document.querySelectorAll('.deleteIcon');
 
-for (let i = 0; i < hideShow.length; i++) {
+for (let i = 0; i < hideShow.length; i+=1) {
   hideShow[i].addEventListener('mouseover', () => {
     trash[i].classList.remove('hidden');
     dots[i].classList.add('hidden');
@@ -40,4 +39,3 @@ for (let i = 0; i < hideShow.length; i++) {
     dots[i].classList.remove('hidden');
   });
 }
-
