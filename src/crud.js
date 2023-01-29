4b21a014a4211b960e637312e05eed9485c7a5a7
Dilsher Balouch <dilsherb@gmail.com>
2023-01-29 15:01:24 +0300

    update todo functionality added.

--- src/crud.js
+++ src/crud.js
@@ -1,6 +1,6 @@
 import { toDoTasks } from './index.js'; // eslint-disable-line
 
-document.querySelector('.fa-angle-down').addEventListener('click', () => {
+const addItem = () => {
   const todoData = document.querySelector('.newTodo').value;
   if (todoData.trim() === '') {
     return;
@@ -9,4 +9,9 @@ document.querySelector('.fa-angle-down').addEventListener('click', () => {
   localStorage.setItem('newTodo', JSON.stringify(toDoTasks));
   window.location.reload();
   document.querySelector('.newTodo').value = '';
+};
+
+document.querySelector('.fa-angle-down').addEventListener('click', addItem);
+document.querySelector('.newTodo').addEventListener('keydown', (e) => {
+  if(e.code === 'Enter'){(addItem())};
 });
